281c169f0bb1818ef473f0bcbc836babf513ec39
Arun Kanaujia <103868399+arunkanaujia23@users.noreply.github.com>
2023-08-23 10:24:45 +0000

    update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let nav__link = document.querySelectorAll('.nav__link');
+let nav__dropdown = document.querySelector('.nav__dropdown')
+
+
+nav__link.forEach((node) => {
+   
+    node.addEventListener('click', () => {
+        nav__dropdown.classList.toggle('nav__dropdown__active')
+      
+    })
+
+})
